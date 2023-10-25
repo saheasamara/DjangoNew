339b518ec8bd5a315f3de4c21605a516e4dfe2a1
esahterova <sahea2021@gmail.com>
2023-10-25 20:01:16 +0400

    work model

--- crm/models.py
+++ crm/models.py
@@ -7,3 +7,8 @@ class Order(models.Model):
     order_dt = models.DateTimeField(auto_now=True)
     order_name = models.CharField(max_length=120)
     order_tel = models.CharField(max_length=20)
+
+
+class MyClass(models.Model):
+    order_dt = models.DateTimeField(auto_now=True)
+    order_name = models.CharField(max_length=120)
